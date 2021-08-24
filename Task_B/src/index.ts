2eb7810b48baa481967fc1164d33b06fa5686d0c
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-24 22:32:16 +0800

    feat: add API end points for todos

--- Task_B/src/index.ts
+++ Task_B/src/index.ts
@@ -1,8 +1,25 @@
 import express from 'express';
-
+import tasks from './tasks.json';
 const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+const taskRouter = express.Router();
+
+taskRouter.get('/:id', (req, res) => {
+  const filteredTasks = tasks.filter((task) => `${task.id}` === `${req.query.id}`);
+  res.send(filteredTasks);
+})
+taskRouter.get('/', (req, res) => res.send(tasks));
+
+taskRouter.post('/', (req, res) => {
+  tasks.push()
+})
+
+taskRouter.delete('/', (req, res) => {
+  res.send("Delete")
+})
+
+app.use('/api/tasks', taskRouter);
 app.listen(port, () => console.log("server started at localhost:8080"));
