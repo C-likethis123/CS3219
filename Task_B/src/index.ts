c7503fa4dd7fffe431cffa0e59182c289503db07
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-24 23:11:45 +0800

    feat: add a connection to mongodb

--- Task_B/src/index.ts
+++ Task_B/src/index.ts
@@ -1,15 +1,24 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 import taskRouter from './api-routes/tasks';
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello World"));
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.use('/api/tasks', taskRouter);
+
+mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+if (db) {
+  console.log("mongodb database connected!")
+}
 
 app.listen(port, () => console.log("server started at localhost:8080"));
