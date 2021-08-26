85dc27434d796c3a5f9de9c2bd44299cac0958fc
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-27 01:56:48 +0800

    fix: refactor api-routes to group http methods by endpoints

--- Task_B/src/api-routes/tasks.ts
+++ Task_B/src/api-routes/tasks.ts
@@ -4,13 +4,12 @@ import taskController from '../controllers/tasks';
 
 const taskRouter = express.Router();
 taskRouter.route('/:id')
-  .get(taskController.getTask);
-  post(taskController.updateTask);
+  .get(taskController.getTask)
+  .post(taskController.updateTask)
+  .delete(taskController.deleteTask);
 
-taskRouter.get('/', taskController.getAllTasks);
-
-taskRouter.post('/', taskController.createTask);
-
-taskRouter.delete('/', taskController.deleteTask);
+taskRouter.route('/')
+  .get(taskController.getAllTasks)
+  .post(taskController.createTask);
 
 export default taskRouter;
