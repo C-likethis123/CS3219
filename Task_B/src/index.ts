6b42a926c89f1622fc158aaafb39efb23404add4
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-03 17:52:20 +0800

    Add CORS support so the frontend app can call backend APIs

--- Task_B/src/index.ts
+++ Task_B/src/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import * as bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import cors from 'cors';
+
 import taskRouter from './api-routes/tasks';
 import errorMiddleware from './validation/errorMiddleware';
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -16,7 +20,7 @@ app.use(bodyParser.json());
 app.use('/api/tasks', taskRouter);
 app.use(errorMiddleware);
 
-mongoose.connect('mongodb://localhost/todos', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://mongo:27017', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 if (db) {
