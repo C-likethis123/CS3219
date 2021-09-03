2a94d9d57c6929cba34ef2c3a5846a06f7c0fda1
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-03 17:50:57 +0800

    Add a submit button to add a task

--- Task_B/frontend/src/components/AddToDo.tsx
+++ Task_B/frontend/src/components/AddToDo.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {makeStyles, TextField} from '@material-ui/core';
+import {makeStyles, TextField, Button} from '@material-ui/core';
+import {KeyboardDatePicker} from '@material-ui/pickers';
 
-const TopBar = () => {
+import useTaskApi from '../api/useTaskApi';
+
+const AddToDo = () => {
   const classes = useStyles();
+  const {createTask} = useTaskApi();
+
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [description, setDescription] = useState('');
+  const createTaskHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const task = {
+      title,
+      date: new Date(date).valueOf(),
+      description,
+    };
+    createTask(task);
+
+  }
+
+  const changeTitle = (event: React.ChangeEvent<HTMLInputElement>) => setTitle(event.target.value);
+  const changeDate = (event: React.ChangeEvent<HTMLInputElement>) => setDate(event.target.value);
+  const changeDescription = (event: React.ChangeEvent<HTMLInputElement>) => setDescription(event.target.value);
   return (
-  <form autoComplete="off" className={classes.root}>
-    <TextField label="Add a task" variant="outlined" />
-  </form>
-)
+    <form autoComplete="off" className={classes.root} onSubmit={createTaskHandler}>
+      <TextField label="Add a task" variant="outlined" value={title} onChange={changeTitle} />
+      <TextField label="Date" format="dd/MM/yyyy" type="date" value={date} onChange={changeDate} />
+      <TextField label="Description" value={description} onChange={changeDescription} />
+      <Button type="submit">Add Task</Button>
+    </form>
+  )
 };
 
 const useStyles = makeStyles((theme) => ({
@@ -20,4 +45,4 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default TopBar;
+export default AddToDo;
