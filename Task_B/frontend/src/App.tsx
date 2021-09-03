c8d6d689dcfb3da3a345cff3ce88ed9c8fca54bb
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-03 17:17:04 +0800

    Retrieve tasks from getTasks handler

--- Task_B/frontend/src/App.tsx
+++ Task_B/frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import TopBar from './components/TopBar';
 import AddToDo from './components/AddToDo';
 
+import useTaskApi from './api/useTaskApi';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const {getAllTasks} = useTaskApi();
+  const getTasksHandler = async () => {
+    const tasks = await getAllTasks();
+    setTasks(tasks);
+  };
+
+  useEffect(() => {
+    getTasksHandler();
+  }, []);
+
   return (
     <div className="App">
       <TopBar />
