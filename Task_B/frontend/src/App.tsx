c7af693e25203219bb2e8c1584205e63e1b2c64d
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-08 14:50:19 +0800

    Make frontend work

--- Task_B/frontend/src/App.tsx
+++ Task_B/frontend/src/App.tsx
@@ -5,29 +5,21 @@ import TopBar from './components/TopBar';
 import AddToDo from './components/AddToDo';
 import TaskDisplay from './components/TaskDisplay';
 
-import {TaskContext, TaskProvider} from './contexts/TaskContext';
-
 function App() {
-  const {tasks, getTasksHandler} = useContext(TaskContext);
-
-  useEffect(() => {
-    getTasksHandler();
-  }, []);
+  const [tasks, setTasks] = React.useState([]);
 
   return (
     <div className="App">
       <TopBar />
-      <TaskProvider>
-        <AddToDo />
-        {tasks.map(({
-          _id,
-          title,
-          date,
-          description,
-          isCompleted,
-        }) => <TaskDisplay key={_id} id={_id} title={title} date={date} description={description} isCompleted={isCompleted} />
-        )}
-      </TaskProvider>
+      <AddToDo />
+      {tasks.map(({
+        _id,
+        title,
+        date,
+        description,
+        isCompleted,
+      }) => <TaskDisplay key={_id} id={_id} title={title} date={date} description={description} isCompleted={isCompleted} />
+      )}
     </div>
   );
 }
