a6cf69a41bc42e83e581304082bc1eff05238ed9
Chow Jia Ying <chowjiaying211@gmail.com>
2021-09-08 15:31:17 +0800

    General refactoring to add constants

--- Task_B/frontend/src/api/useTaskApi.ts
+++ Task_B/frontend/src/api/useTaskApi.ts
@@ -1,25 +1,26 @@
 import React from 'react';
 import axiosInstance from './axiosConfig';
 
-
 type Task = {
   id?: string
   title: string
 };
 
+const API_PREFIX = '/api/tasks';
+
 const useTaskApi = () => {
   const getAllTasks = async () => {
-    const {data: {data: tasks}} = await axiosInstance.get('/api/tasks/');
+    const {data: {data: tasks}} = await axiosInstance.get(API_PREFIX);
     return tasks;
   }
 
   const createTask = async (task: Task) => {
-    const {data: createdTask} = await axiosInstance.post('/api/tasks/', task);
+    const {data: createdTask} = await axiosInstance.post(API_PREFIX, task);
     return createdTask;
   }
 
   const deleteTask = async (id: string) => {
-    const {data: {data: deletedId}} = await axiosInstance.delete(`api/tasks/${id}`);
+    const {data: {data: deletedId}} = await axiosInstance.delete(`${API_PREFIX}/${id}`);
     return deletedId;
   }
   return {getAllTasks, createTask, deleteTask};
